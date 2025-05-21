fb3539b89935e7159c8296451bd0a2c831143e07
Cristian Alcantar <cristron2014@hotmail.com>
2025-05-21 09:03:43 -0700

    Agregando gitignore (dev)

--- src/Alumnos/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ src/Alumnos/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a29ea4f202f76ad19f020cda088f922e41ffaeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
